9aefc57413398407e1f75a110f7efbd6069d6c0a
Tom Stovall <stovak@gmail.com>
2019-12-10 10:12:15 -0800

    Sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ const gulp  = require('gulp');
 const shell = require('gulp-shell');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+
 
 var drupalCoreBuild = shell.task('cd web/core && npm install && npm run build');
 
@@ -9,8 +11,10 @@ var drupalCoreBuild = shell.task('cd web/core && npm install && npm run build');
 function themeBuild() {
   return gulp.src("./web/themes/custom/milken/scss/milken.scss")
     .pipe(sourcemaps.init())
+    .pipe(autoprefixer())
     .pipe(sass()).on('error', sass.logError)
-    .pipe(gulp.dest('./web/themes/custom/milken/css'));
+    .pipe(sourcemaps.write("../css"))
+    .pipe(gulp.dest('../css'));
 }
 
 exports.themeBuild = themeBuild;
