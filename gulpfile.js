089b0416d2b89cc5e82350d86ea63db3b38017e1
Tom Stovall <stovak@gmail.com>
2019-12-10 09:58:07 -0800

    Build CSS/JS pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,22 @@
-const { src, dest, parallel } = require('gulp');
-const shelljs = require('shelljs');
+const gulp  = require('gulp');
+const shell = require('gulp-shell');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
 
-
-function drupalCoreBuild() {
-  return shelljs.exec('cd web/core && npm install && npm run build');
-}
+var drupalCoreBuild = shell.task('cd web/core && npm install && npm run build');
 
 
 function themeBuild() {
-  console.log("theme build");
-  return true;
+  return gulp.src("./web/themes/custom/milken/scss/milken.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass()).on('error', sass.logError)
+    .pipe(gulp.dest('./web/themes/custom/milken/css'));
 }
 
 exports.themeBuild = themeBuild;
 exports.drupalCoreBuild = drupalCoreBuild;
 
-exports.default = parallel(themeBuild, drupalCoreBuild);
+exports.default = gulp.parallel(themeBuild, drupalCoreBuild);
 
 
 
