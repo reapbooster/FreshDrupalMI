88f0e2d54fba90b2496f0147e8d2cd2ca33fbaa2
Tom Stovall <stovak@gmail.com>
2019-12-11 16:07:23 -0800

    Update gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,16 @@ const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
 const concat = require("gulp-concat");
 
-var drupalCoreBuild = shell.task('cd web/core && npm install && npm run build');
-var clearDrupalCache = shell.task('drush cr');
+function typescriptCompileCallback(error, stdout, stderr) {
+  console.error(stderr);
+  console.log(stdout);
+  console.error(error);
+};
 
-function themeBuild() {
+gulp.task('drupalCoreBuild', shell.task('cd web/core && npm install && npm run build'));
+gulp.task('clearDrupalCache', shell.task('drush cr'))
+gulp.task('tsCompile',shell.task('tsc --esModuleInterop --resolveJsonModule'));
+gulp.task('themeBuild', () => {
   return gulp.src("/var/www/web/themes/custom/milken/scss/milken.scss")
     .pipe(sourcemaps.init())
     .pipe(autoprefixer())
@@ -16,22 +22,18 @@ function themeBuild() {
     .on('error', sass.logError)
     .pipe(sourcemaps.write("../css"))
     .pipe(gulp.dest('/var/www/web/themes/custom/milken/css'));
-}
+});
 
-function mergePackageJsonFiles() {
-  return gulp.src("./**/package.json")
+gulp.task('mergePackgageJsonFiles', (done) => {
+  gulp.src("./**/package.json")
     .on('error', sass.logError)
-    .pipe(concat('packages.json'))
-    .pipe(gulp.dest('dist.json'));
-}
-
-exports.mergePackageJsonFiles = mergePackageJsonFiles;
-exports.themeBuild = themeBuild;
-exports.drupalCoreBuild = drupalCoreBuild;
-exports.clearDrupalCache = clearDrupalCache;
-
-exports.default = gulp.parallel(themeBuild, drupalCoreBuild);
+    .pipe(() => {
+      console.log(arguments);
+    });
+  return done();
+});
 
 
+gulp.task('default', gulp.series(['tsCompile', 'drupalCoreBuild', 'themeBuild']))
 
 
