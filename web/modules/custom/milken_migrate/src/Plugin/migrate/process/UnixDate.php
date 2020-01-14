db2a785867a43b5590589a60f8fc02d718d1e0df
Tom Stovall <stovak@gmail.com>
2020-01-13 17:45:31 -0800

    first 50 article import works

---
+++ web/modules/custom/milken_migrate/src/Plugin/migrate/process/UnixDate.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\milken_migrate\Plugin\migrate\process;
+
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\Row;
+
+/**
+ * Turn any date field to unix timestamp.
+ *
+ * @code
+ * changed:
+ *   plugin: milken_migrate:unix_date
+ *   source: changed
+ * @endcode
+ *
+ * @MigrateProcessPlugin(
+ *   id = "milken_migrate:unix_date"
+ * )
+ */
+class UnixDate extends ProcessPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+    $dt = new \DateTime();
+    if (!empty($value)) {
+      $dt = \DateTime::createFromFormat(\DateTimeInterface::W3C, $value);
+    }
+    if ($dt instanceof \DateTime) {
+      return $dt->getTimestamp();
+    }
+    return NULL;
+  }
+
+}
