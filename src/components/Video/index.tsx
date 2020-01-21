34a12b40e96d66d9b2ce5544f385bdf0aa46a90a
Tom Stovall <stovak@gmail.com>
2020-01-20 18:46:13 -0800

    Adding report migration

---
+++ src/components/Video/index.tsx
@@ -0,0 +1,19 @@
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { graphql, Link } from "gatsby";
+
+class Video extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
+
+}
+
+
+export default Video;
