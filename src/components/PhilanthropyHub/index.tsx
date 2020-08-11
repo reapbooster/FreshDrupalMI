b2243e5489601a5c9070793ed893869495d6feb0
Klemen Novak <knovak@eis-202003-01m.ad.execproinc.com>
2020-08-11 00:27:31 -0700

    1st pass at horizontal filter

--- src/components/PhilanthropyHub/index.tsx
+++ src/components/PhilanthropyHub/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import JSONApiUrl from "../../DataTypes/JSONApiUrl";
-import {Container, Nav, Tab} from "react-bootstrap";
+import { Col, Container, Nav, Row, Tab } from "react-bootstrap";
 import ListComponentProps from "../../DataTypes/ListComponentProps";
 import DropdownFacet from "../List/DropdownFacet";
 import HorizontalMenuFacet from "../List/HorizontalMenuFacet";
@@ -11,6 +11,7 @@ const PhilanthropyHub: React.FunctionComponent = (props) => {
 
   var url = new JSONApiUrl(props.url);
   var all = (
+
     <Nav
       defaultActiveKey="all"
       variant={"pills"}
@@ -47,29 +48,39 @@ const PhilanthropyHub: React.FunctionComponent = (props) => {
       <DropdownFacet
         type={"taxonomy_term--ph_region"}
         id={props.id.concat("-region")}
-        label="ph_region"
+        label={"Region"}
+        urlParam={"ph_region"}
         field={"field_region"}
         url={"/jsonapi/taxonomy_term/ph_region?jsonapi_include=true"}
       />
-      <Tab.Container className={"d-flex justify-content-center"}>
-        {all}
-        <HorizontalMenuFacet
-          url={"/jsonapi/taxonomy_term/ph_focus?jsonapi_include=true"}
-          id={props.id.concat("-field_focus")}
-          type={"taxonomy_term--ph_focus"}
-          label={"ph_focus"}
-          field={"field_focus"}
-          all={all}
-        />
-        <HorizontalMenuFacet
-          url={"/jsonapi/taxonomy_term/ph_actions?jsonapi_include=true"}
-          id={props.id.concat("-field_actions")}
-          type={"taxonomy_term--ph_actions"}
-          label={"ph_actions"}
-          field={"field_actions"}
-          all={all}
-        />
-      </Tab.Container>
+
+      <Container>
+        <Row className="d-flex justify-content-md-center">
+          <Col>
+            <HorizontalMenuFacet
+              url={"/jsonapi/taxonomy_term/ph_focus?jsonapi_include=true"}
+              id={props.id.concat("-field_focus")}
+              type={"taxonomy_term--ph_focus"}
+              label={"Focus"}
+              urlParam={"ph_focus"}
+              field={"field_focus"}
+              all={all}
+            />
+          </Col>
+          <Col>
+            <HorizontalMenuFacet
+              url={"/jsonapi/taxonomy_term/ph_actions?jsonapi_include=true"}
+              id={props.id.concat("-field_actions")}
+              type={"taxonomy_term--ph_actions"}
+              label={"Actions"}
+              urlParam={"ph_actions"}
+              field={"field_actions"}
+              all={all}
+            />
+          </Col>
+        </Row>
+      </Container>
+
       <ListComponentProps {...props} url={url} />
     </Container>
   );
