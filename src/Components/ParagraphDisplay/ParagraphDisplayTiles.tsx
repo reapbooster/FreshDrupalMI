4aee9d85906056eb6e8ed08b6328a7be094b6999
Tom Stovall <stovak@gmail.com>
2020-11-06 10:33:13 -0800

    rewrite as class to add onclick handler for scroll

--- src/Components/ParagraphDisplay/ParagraphDisplayTiles.tsx
+++ src/Components/ParagraphDisplay/ParagraphDisplayTiles.tsx
@@ -5,46 +5,84 @@ import { EntityComponentProps } from "../../DataTypes/EntityComponentProps";
 import Loading from "../Loading";
 import ParagraphDataFactory from "./ParagraphDataFactory";
 import ListDisplay from "../ListDisplay";
+import Paragraph, { ParagraphInterface } from "../../DataTypes/Paragraph";
 
 export interface ParagraphDisplayTilesProps {
   data: ParagraphTilesInterface;
 }
 
-export const ParagraphDisplayTiles = (props: ParagraphDisplayTilesProps) => {
-  const { data } = props;
-  console.debug("ParagraphDisplayTiles => initial data:", data);
-  const DataObject = ParagraphDataFactory(data);
-  const [paragraphData, setParagraphData] = useState(DataObject);
-  if (!paragraphData.hasData()) {
-    console.debug("Paragraph does not have data", paragraphData);
-    const ecp = new EntityComponentProps(paragraphData);
-    ecp
-      .getData(paragraphData.getIncluded())
-      .then((res) => res.json())
-      .then((ajaxData) => {
-        console.debug("ParagraphDisplayTiles", ajaxData);
-        const returnedData = ParagraphDataFactory(data);
-        setParagraphData(returnedData);
-      });
-    return (
-      <>
-        <Loading />
-      </>
-    );
+export interface ParagraphDisplayTilesState {
+  data: Paragraph;
+  loading: boolean;
+  loaded: boolean;
+}
+
+export class ParagraphDisplayTiles extends React.Component<
+  ParagraphDisplayTilesProps,
+  ParagraphDisplayTilesState
+> {
+  constructor(props) {
+    super(props);
+    const { data } = props;
+    const DataObject = ParagraphDataFactory(data);
+    this.state = {
+      data: DataObject,
+      loading: false,
+      loaded: DataObject.hasData(),
+    };
+    this.scrollOnClickHandler = this.scrollOnClickHandler.bind(this);
   }
-  console.log("paragraph display tiles should have data:", paragraphData);
-  // TODO: make this a flex box that holds to 100% and hides anything offscreen
-  return (
-    <Container 
-      fluid={(props.data.field_view_mode == "card") ? true : false }
-      className={(props.data.field_view_mode == "card") ? "position-relative" : "" } >
-      <ListDisplay
-        id={"tiles-list-".concat(paragraphData.id)}
-        list={paragraphData.tiles}
-        view_mode={paragraphData.field_view_mode}
-      />
-    </Container>
-  );
-};
+
+  scrollOnClickHandler() {
+    // Scoll on click handler goes here
+  }
+
+  componentDidMount() {
+    const { data, loading } = this.state;
+    if (!data.hasData() && !loading) {
+      console.debug("Paragraph does not have data", data);
+      this.setState({ loaded: false, loading: true });
+      const ecp = new EntityComponentProps(data);
+      ecp
+        .getData(data.getIncluded())
+        .then((res) => res.json())
+        .then((ajaxData) => {
+          const returnedData = ParagraphDataFactory(ajaxData.data);
+          this.setState({
+            loaded: true,
+            loading: false,
+            data: returnedData,
+          });
+        });
+    }
+  }
+
+  render() {
+    const { data, loading, loaded } = this.state;
+    if (loading) {
+      return (
+        <>
+          <Loading />
+        </>
+      );
+    }
+    if (loaded) {
+      return (
+        <Container
+          fluid={data.field_view_mode == "card" ? true : false}
+          className={data.field_view_mode == "card" ? "position-relative" : ""}
+        >
+          <ListDisplay
+            id={"tiles-list-".concat(data.id)}
+            list={data.tiles}
+            view_mode={data.field_view_mode}
+            onClickHandler={this.scrollOnClickHandler}
+          />
+        </Container>
+      );
+    }
+    return <div />;
+  }
+}
 
 export default ParagraphDisplayTiles;
