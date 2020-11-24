5659ac4a636fbb5e7d356cb687c4b9a5ffa26eeb
Tom Stovall <stovak@gmail.com>
2020-11-23 20:58:01 -0800

    Fixing issue with LiveDataFixture

--- src/Utility/LiveDataFixture.ts
+++ src/Utility/LiveDataFixture.ts
@@ -14,16 +14,20 @@ export class LiveDataFixture {
   }
 
   async getFixtureData(include = ""): Promise<Array<EntityInterface>> {
-    return fetch(
-      `${process.env.NODE_TESTING_URL}/jsonapi/${this.entityTypeId}/${this.bundle}?jsonapi_include=true&`.concat(
+    return fetch( this.getBaseUrl().concat(
+      `/jsonapi/${this.entityTypeId}/${this.bundle}?jsonapi_include=true&`.concat(
         include
-      )
+      ))
     )
       .catch((err) => {
         console.error(err.message);
         process.exit(err.code);
       })
       .then((res) => res.json());
+  }
+
+  getBaseUrl() {
+    return process.env.NODE_TESTING_URL ?? "https://live-freshdrupalmi.pantheonsite.io/"
   }
 }
 
