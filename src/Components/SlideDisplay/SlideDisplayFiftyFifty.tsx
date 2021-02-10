55eb4a15e8889929cefc1d1a9c55135ec2400891
Jose Ramirez <webmaster@reapbooster.com>
2021-02-10 22:07:55 +0000

    Fix Slide components

--- src/Components/SlideDisplay/SlideDisplayFiftyFifty.tsx
+++ src/Components/SlideDisplay/SlideDisplayFiftyFifty.tsx
@@ -1,8 +1,13 @@
 import React, { useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import { SlideInterface } from "DataTypes/Slide";
 import { SlideFiftyFifty } from "DataTypes/SlideFiftyFifty";
 import { EntityComponentProps } from "DataTypes/EntityComponentProps";
 import Loading from "../Loading";
+import { ErrorBoundary } from "../../Utility/ErrorBoundary";
+import { ImageFile } from "../../DataTypes/ImageFile";
+import { KeyValueTextFieldDisplay } from "../../Fields/KeyValueTextFieldDisplay";
+import styled from "styled-components";
 
 export interface SlideDisplayFiftyFiftyProps {
   data: SlideInterface;
@@ -12,7 +17,7 @@ export interface SlideDisplayFiftyFiftyProps {
 export const SlideDisplayFiftyFifty: React.FunctionComponent = (
   props: SlideDisplayFiftyFiftyProps
 ) => {
-  console.debug("FiftyFifty", props);
+  console.debug("SlideDisplayFiftyFifty Component Start", props);
   const { data, view_mode } = props;
   const DataObject = new SlideFiftyFifty(data);
   const [slideData, setSlideData] = useState(DataObject);
@@ -27,12 +32,81 @@ export const SlideDisplayFiftyFifty: React.FunctionComponent = (
       });
     return <Loading />;
   }
-  const leftOrRight = slideData.type.split("_").pop();
   console.log("Slide Fifty Fifty Data:", slideData);
+  
+  const backgroundImage =
+    slideData.field_background_image instanceof ImageFile
+      ? slideData.field_background_image
+      : new ImageFile(slideData.field_background_image);
+
+  const textLeftOrRight = slideData.type.split("_").pop();
+  const colOrder = (textLeftOrRight === "left") ? "align-items-center align-items-stretch flex-row-reverse" : "align-items-center align-items-stretch";
+
+  const textLineContainer = styled.div``;
+  
+  const colStyleText = {
+    backgroundColor: `${slideData.field_background_color?.color}`,
+    margin: 0,
+    padding: '8vw',
+    minHeight: '24em',
+    height: '33vw',
+  };
+
+  const colStyleImage = {
+    backgroundColor: `${slideData.field_background_color?.color}`,
+    backgroundImage: `url(${backgroundImage.imageStyleObject.large})`,
+    backgroundSize: 'cover',
+    margin: 0,
+    padding: '8vw',
+    minHeight: '24em',
+    height: '33vw',
+  };
+
+  const textLines = (
+    <KeyValueTextFieldDisplay
+      Container={textLineContainer}
+      style={{ color: `${slideData.field_text_color?.color}` }}
+      data={slideData.field_slide_text}
+    />
+  );
+
+  const slideLink = (slideData.field_link?.title && slideData.field_link?.uri) ? (
+    <a
+    href={`${slideData.field_link?.uri || "#"}`}
+    style={{
+      background: "var(--color-milken-orange)",
+      color: "white",
+      fontWeight: "bold",
+      letterSpacing: "0.1em",
+      padding: "1em",
+      textDecoration: "none",
+      textTransform: "uppercase",
+    }}
+  >
+    {`${slideData.field_link?.title || "View More"}`}
+  </a>
+  )
+  :
+  '';
+
+  console.debug(textLines);
+  
   return (
-    <div>
-      <h3>Slide--Fifty-Fifty -{leftOrRight}</h3>
-    </div>
+    <>
+      <ErrorBoundary>
+        <Container fluid>
+          <Row className={colOrder} data-view-mode={view_mode} >
+            <Col md={6} className="align-items-stretch" style={colStyleImage} >
+            </Col>
+            <Col md={6} className="" style={colStyleText} >
+              {textLines}
+              {slideLink}
+            </Col>
+          </Row>
+        </Container>
+        
+      </ErrorBoundary>
+    </>
   );
 };
 
--- src/Components/SlideDisplay/SlideDisplayFullWidthOneColumn.tsx
+++ src/Components/SlideDisplay/SlideDisplayFullWidthOneColumn.tsx
@@ -50,18 +50,26 @@ export const SlideDisplayFullWidthOneColumn: React.FunctionComponent = (
     padding: 0,
   };
 
-  const slideTextStyle = {
+  const isHeroImage = (slideData.field_is_hero_image === undefined || slideData.field_is_hero_image === true)? true : false;
+
+  const slideTextStyle = (isHeroImage)?
+  {
     top: "50%",
     transform: "translateY(-50%)",
     display: "block",
     position: "absolute",
     paddingLeft: "7em",
+  }
+  :
+  {
+    paddingTop: "5em",
+    paddingBottom: "5em",
   };
 
   console.debug("slideData Var", slideData);
   console.debug("background image object: ", backgroundImage);
   const Jumbotron = styled.div`
-    min-height: 650px;
+    min-height: ${(isHeroImage)? "650px" : ""};
     width: 100%;
     background-position: center;
     background-clip: border-box;
@@ -85,19 +93,25 @@ export const SlideDisplayFullWidthOneColumn: React.FunctionComponent = (
     />
   );
 
-  if (slideData.field_link?.title && slideData.field_link?.uri) {
-    textLines.push(
-      <p key={textLines.length + 1}>
-        <a
-          href={`${slideData.field_link?.uri || "#"}`}
-          className="btn btn-primary btn-lg"
-          style={{ color: `${slideData.field_text_color?.color} || #000000` }}
-        >
-          {data.field_link?.title || "#"}
-        </a>
-      </p>
-    );
-  }
+
+  const slideLink = (slideData.field_link?.title && slideData.field_link?.uri) ? (
+    <a
+    href={`${slideData.field_link?.uri || "#"}`}
+    style={{
+      background: "var(--color-milken-orange)",
+      color: "white",
+      fontWeight: "bold",
+      letterSpacing: "0.1em",
+      padding: "1em",
+      textDecoration: "none",
+      textTransform: "uppercase",
+    }}
+  >
+    {`${slideData.field_link?.title || "View More"}`}
+  </a>
+  )
+  :
+  '';
 
   console.debug(textLines);
 
@@ -111,7 +125,7 @@ export const SlideDisplayFullWidthOneColumn: React.FunctionComponent = (
           data-view-mode={view_mode}
         >
           <Jumbotron className="jumbotron jumbotron-fluid d-block align-items-center m-0 p-0">
-            <Container style={slideTextStyle}>{textLines}</Container>
+            <Container style={slideTextStyle}>{textLines}{slideLink}</Container>
           </Jumbotron>
         </Row>
       </ErrorBoundary>
