c95b65462f430f11c2e50e44d8588298252515f8
Jose Ramirez <webmaster@reapbooster.com>
2021-04-01 23:54:01 +0000

    EventFeatureTiles: Improve layout and fix bugs :art:

--- src/Components/NodeDisplay/LandingPage/NodeDisplayLandingPageCardLarge.tsx
+++ src/Components/NodeDisplay/LandingPage/NodeDisplayLandingPageCardLarge.tsx
@@ -7,46 +7,6 @@ import ErrorBoundary from "Utility/ErrorBoundary";
 import MediaDisplayImage from "Components/MediaDisplay/MediaDisplayImage";
 import NodeLandingPage from "DataTypes/NodeLandingPage";
 
-const CardOuter = styled.a`
-  padding: 1em;
-
-  & :hover {
-    text-decoration: none;
-  }
-
-  & .card-container {
-    padding: 0;
-    cursor: pointer;
-    text-decoration: none;
-    width: 100%;
-    position: relative;
-  }
-
-  & .card-container:hover {
-    box-shadow: 0 8px 16px 0 grey;
-  }
-
-  & .card-container:hover .card-title {
-    color: var(--color-milken-orange) !important;
-  }
-  & .card-container:hover .card-body div {
-    display: unset;
-  }
-`;
-
-const CardLinkBox = styled.div`
-  display: none;
-  position: absolute;
-  bottom: 0px;
-  left: 0px;
-  width: 100%;
-  border-bottom: 4px solid var(--color-milken-orange);
-`;
-
-const onClickHandler = (evt) => {
-  console.debug("onClickHandler", evt);
-  document.location.href = evt.currentTarget.dataset.alias;
-};
 
 export interface NodeDisplayLandingPageCardLargeProps {
   data: NodeLandingPage;
@@ -57,10 +17,67 @@ export const NodeDisplayLandingPageCardLarge = (
   props: NodeDisplayLandingPageCardLargeProps
 ) => {
   const { data, key } = props;
+  
+  const CardOuter = styled.a`
+    padding: 1em;
+
+    & :hover {
+      text-decoration: none;
+    }
+
+    & .card-container {
+      padding: 0;
+      cursor: pointer;
+      text-decoration: none;
+      width: 100%;
+      position: relative;
+
+      & .card-body {
+        position: relative;
+      }
+    }
+
+    & .card-container:hover {
+      box-shadow: 0 8px 16px 0 grey;
+    }
+
+    & .card-container:hover .card-title {
+      color: var(--color-milken-orange) !important;
+    }
+    & .card-container:hover .card-body div {
+      display: unset;
+    }
+  `;
+
+  const CardDateLabel = styled.div`
+    background-color: #9a6397;
+    bottom: 0px;
+    color: #fff;
+    display: ${(data.field_tile_label == null) ? 'none !important' : 'block'};
+    font-family: LatoWebBold;
+    font-size: 1em;
+    height: 2em;
+    left: 0px;
+    line-height: 1em;
+    padding: 0.5em 1em;
+    position: absolute;
+
+    @media (min-width: 768px) and (max-width: 1199.98px) {
+      font-size: 0.8em;
+    }
+  `;
+
+  const CardLinkBox = styled.div`
+    display: none;
+    position: absolute;
+    bottom: 0px;
+    left: 0px;
+    width: 100%;
+    border-bottom: 4px solid var(--color-milken-orange);
+  `;
+
   return (
     <CardOuter
-      // onClick={onClickHandler}
-      // data-alias={data.path.alias}
       href={data.path.alias !== null ? data.path.alias : '/node/' + data.drupal_internal__nid}
       key={key}
       className="card border-0 col-sm-12 col-md-4"
@@ -72,6 +89,7 @@ export const NodeDisplayLandingPageCardLarge = (
               data={data.field_hero_image}
               view_mode={"medium-raw"}
             />
+            <CardDateLabel>{data.field_tile_label}</CardDateLabel>
           </ErrorBoundary>
           <CardLinkBox>
             <FontAwesomeIcon
