449c99a1fc055f583a16689c342a44d966671934
Jose Ramirez <webmaster@reapbooster.com>
2021-04-01 15:49:28 +0000

    People Staff Profiles improved :boom:

--- src/Components/PeopleDisplay/PeopleFullDisplay.tsx
+++ src/Components/PeopleDisplay/PeopleFullDisplay.tsx
@@ -4,70 +4,61 @@ import styled from "styled-components";
 import { TagsDisplay } from "../TagsDisplay"
 import { SocialDisplay } from "../SocialDisplay"
 
-const PersonFullDisplay = ( props: any ) => {
-  
+const PersonFullDisplay = (props: any) => {
+
   const { data } = props;
-  
+
   const PersonElMainWrapper = styled.div`
-  `;
+    & .hero-wrapper {
+      background: var(--color-milken-blue);
 
-  const HeroWrapper = styled.div`
-    background: var(--color-milken-blue);
-    width: 100%;
-    & .nameTitle {
-      display: flex;
-      align-items: center;
-      justify-content: flex-end;
-      padding: 0;
-    }
-
-    @media only screen and (max-width: 767.98px) {
-      padding-top: 55.25%;
-    }
-  `;
-
-  const ElMainContentWrapper = styled.div`
-    & .section-social {
-      order: 1;
-    }
-    & .section-content {
-      order: 2;
-      @media only screen and (max-width: 1199.98px) {
-        order: 3;
-        padding-top: 1.5em;
-
+      @media only screen and (max-width: 768px) {
+        & .justify-content-end {justify-content: center !important;}
+        & img {width: 100%;}
       }
-    }
-    & .section-tags {
-      order: 3;
-      @media only screen and (max-width: 1199.98px) {
-        order: 2;
-      }
-    }
-  `;
-
-  const NameTitle = styled.div`
-    color: #fff;
-    width: 450px;
-    margin: 0px 106px 0 113px;
-    padding: 40px 20px 37px 20px;
-
-    & a {
-      color: var(--color-milken-orange);
-      letter-spacing: 2px;
-      font-family: 'LatoWebBlack';
-      text-transform: uppercase;
-      font-size: 0.9em;
-    }
-
-    & h1 {
-      font-family: 'LatoWebBlack';
-      font-size: 3rem;
-      margin: 27px 0 18px;
-    }
+      
+      & .name-title {
+        color: #fff;
+        max-width: 30em;
     
-    & h5 {
-      font-size: 1.5em;
+        & a {
+          color: var(--color-milken-orange);
+          letter-spacing: 2px;
+          font-family: 'LatoWebBlack';
+          font-size: 0.9em;
+          text-transform: uppercase;
+        }
+    
+        & h1 {
+          font-family: 'LatoWebBlack';
+          font-size: 3rem;
+          margin: 27px 0 18px;
+        }
+        
+        & h5 {
+          font-size: 1.5em;
+        }
+      }
+    }
+
+    & .main-wrapper {
+      & .section-social {
+        order: 1;
+      }
+
+      & .section-content {
+        order: 2;
+        @media only screen and (max-width: 992px) {
+          order: 3;
+        }
+      }
+
+      & .section-tags {
+        order: 3;
+        @media only screen and (max-width: 992px) {
+          order: 2;
+        }
+      }
     }
   `;
 
@@ -75,53 +66,43 @@ const PersonFullDisplay = ( props: any ) => {
   if (data.field_teams.length !== undefined && data.field_teams.length > 0) {
     data.field_teams.map(
       (item) => {
-        teamList.push({link_uri: '', tag: item.name});
+        teamList.push({ link_uri: '', tag: item.name });
       }
     );
   }
 
   return (
-    <PersonElMainWrapper className="container-fluid p-0">
-      <Row className="no-gutters">
-        <Col>
-          <HeroWrapper>
-            <Row>
-              <Col lg="6" className="nameTitle">
-                <NameTitle>
-                  <a href="#" onClick={() => { history.back() }}>&lt; Back to previous page</a>
-                  <h1>{data.field_first_name} {data.field_last_name}</h1>
-                  <h5>{data.field_pgtitle}</h5>
-                </NameTitle>
-              </Col>
-              <Col lg="6">
-                <img src={data?.field_photo[0]?.uri?.url} />
-              </Col>
-            </Row>
-          </HeroWrapper>
+    <PersonElMainWrapper className="container-fluid">
+      <Row className="hero-wrapper">
+        <Col md="6" sm="12" className="d-flex align-items-center justify-content-end p-0 pr-xl-5">
+          <div className="name-title p-5 mx-lg-5">
+            <a href="#" onClick={() => { history.back() }}>&lt; Back to previous page</a>
+            <h1>{data.field_first_name} {data.field_last_name}</h1>
+            <h5>{data.field_pgtitle}</h5>
+          </div>
+        </Col>
+        <Col md="6" sm="12" className="p-0">
+          <img className="img-fluid" src={data?.field_photo[0]?.uri?.url} />
         </Col>
       </Row>
-      <Row>
-        <ElMainContentWrapper className="container-fluid" style={{ width: "90%", margin: "2em auto" }}>
-          <Row>
-            <Col xs="12" lg="6" xl="1" className="section-social">
-              <SocialDisplay data={{ "name": data.name }}></SocialDisplay>
-            </Col>
-            <Col xs="12" xl="8" className="section-content">
-              <p>{data.field_biotext}</p>
-            </Col>
-            <Col xs="12" lg="6" xl="3" className="section-tags">
-              <TagsDisplay data={
-                {
-                  published_date_string: "",
-                  tagList: teamList
-                }
-              }></TagsDisplay>
-            </Col>
-          </Row>
-        </ElMainContentWrapper>
+      <Row className="main-wrapper py-4 mx-lg-5">
+        <Col sm="6" lg="1" className="section-social pt-3">
+          <SocialDisplay data={{ "name": data.name }}></SocialDisplay>
+        </Col>
+        <Col lg="8" className="section-content p-3 px-4 px-md-5">
+          <p className="text-justify" dangerouslySetInnerHTML={{ __html: data.field_biotext }}></p>
+        </Col>
+        <Col sm="6" lg="3" className="section-tags pt-3">
+          <TagsDisplay data={
+            {
+              published_date_string: "",
+              tagList: teamList
+            }
+          }></TagsDisplay>
+        </Col>
       </Row>
     </PersonElMainWrapper>
   );
-  
-            };
+
+};
 export default PersonFullDisplay;
