c9307aae44e046180c1f0df8ef28c50ee6204857
Klemen Novak <knovak@execproinc.com>
2021-04-02 16:15:38 -0700

    Link bar - react key missing

--- src/Components/ParagraphDisplay/ParagraphDisplayLinkBar.tsx
+++ src/Components/ParagraphDisplay/ParagraphDisplayLinkBar.tsx
@@ -69,7 +69,7 @@ const ParagraphDisplayLinkBar: React.FunctionComponent = (
           let activeLinkClass =
             window.location.pathname == item.uri ? "active" : "";
           return (
-            <Col className={activeLinkClass}>
+            <Col className={activeLinkClass} key={key}>
               <a data-link-uri={item.uri} href={item.uri}>
                 <h2 className="text-center text-uppercase m-0">{item.title}</h2>
               </a>
