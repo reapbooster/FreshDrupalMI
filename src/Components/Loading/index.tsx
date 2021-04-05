3163ec65603d709d1600d7196ec3354520e17ab7
Jose Ramirez <webmaster@reapbooster.com>
2021-04-05 21:43:59 +0000

    Add custom Milken Themed loading spinner :gem:

--- src/Components/Loading/index.tsx
+++ src/Components/Loading/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import FontAwesomeIcon from "react-fontawesome";
 import { Col, Container, Row } from "react-bootstrap";
 
 const Loading = () => {
@@ -7,10 +6,7 @@ const Loading = () => {
     <Container>
       <Row>
         <Col lg={12}>
-          <figure style={{ margin: "20px auto" }}>
-            <FontAwesomeIcon name="spinner" size={"5x"} />
-            <figcaption>Loading...</figcaption>
-          </figure>
+          <img src='/sites/default/files/MilkenLoadingSpinner.gif' alt='loading animation' />
         </Col>
       </Row>
     </Container>
